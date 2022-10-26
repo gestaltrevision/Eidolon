8960aeb6dfca829d2d243b4db3467149b85b8c72
Amir Shokri <amirsh.nll@gmail.com>
2022-10-27 00:18:31 +0330

    fix if tmp == None //  ruth value of an array with more than one element is ambiguous bug

--- eidolon/helpers.py
+++ eidolon/helpers.py
@@ -245,9 +245,10 @@ def StackDisarray(aDOGScaleSpace, xDisplacements, yDisplacements, reach):
     sd = StackDisarrayGenerator(aDOGScaleSpace, xDisplacements, yDisplacements, reach)
     tmp = None  
     for plane in sd:
-        if tmp == None:
+        if tmp is None:
             tmp = np.zeros(plane.shape)
-        tmp += plane
+        else:
+            tmp += plane
     return tmp
 
 
